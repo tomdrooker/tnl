026ce041791f18a49a25bfeb6a4c6dfd62325a18
Tom Rooker <tomdrooker@gmail.com>
2019-11-26 13:40:06 +0000

    Initial commit

---
+++ app/routes.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+
+// Add your routes here - above the module.exports line
+
+/*router.post("/GGPath/signIn", function(req, res) {
+  var form = req.session.data["userId"];
+  console.log(form);
+});*/
+
+module.exports = router
